e312df8fbdad66b651f07b02474f53594c079601
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-03-15 23:21:27 -0400

    removed logger and logs

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -55,7 +55,6 @@ function Player(props) {
 				}
 			});
 			setIsCorrectGuess(correctGuessArr);
-			console.log(correctGuessArr);
 		}
 	}
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 //React
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 //Redux
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import { reducer, initialState } from "./reducers";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { reducer, initialState } from './reducers';
 
 //App
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer, initialState, applyMiddleware(logger));
+const store = createStore(reducer, initialState, applyMiddleware());
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
